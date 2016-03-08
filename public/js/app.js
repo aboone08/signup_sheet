c23b15e0274e7a933f60f29f2b426ff319a14074
aboone08 <andranique.boone@gmail.com>
2016-03-07 20:20:06 -0500

    crap

--- public/js/app.js
+++ public/js/app.js
@@ -1,68 +1,39 @@
 var app = angular.module('ng_signUp', []);
 
-app.factory('entry', function(){
+app.controller('EntryController', function($scope){
+  // var data = memberName + itemEntered + checkbox
   return[
     {'memberName': 'Sister Ivy', 'item': "food", "checkbox": "day"},
   ];
-});
 
-app.controller('EntryController', function($scope){
-  var entry = memberName + itemEntered + checkbox
-  $scope.entry = entry;
+
+  $scope.entry = 'entry';
   $scope.selectedEntry = null;
   $scope.entryCopy = null;
+  $scope.checkbox = false;
 
-  $scope.selectedEntry = function(entry){
-    $scope.selectedEntry = entry;
-    $scope.entryCopy = angular.copy(entry);
-  }
-
-
-  $scope.saveEntry = function(){
-    $scope.selectedEntry.entry = $scope.entryCopy.entry;
-  }
-
-  // $scope.checkbox = {
-  //   Su : '',
-  //   M  : '',
-  //   Tu : '',
-  //   W  : '',
-  //   Th : '',
-  //   F  : '',
-  //   Sa : ''
+  // $scope.selectedEntry = function(entry){
+  //   $scope.selectedEntry = entry;
+  //   $scope.entryCopy = angular.copy(entry);
   // }
 
-  $scope.checkbox('click', [$scope, function(){
-      var Su = element(by.binding('checkbox.Su'));
-      var M = element(by.binding('checkbox.M'));
-      var T = element(by.binding('checkbox.Tu'));
-      var W = element(by.binding('checkbox.W'));
-      var Th = element(by.binding('checkbox.Th'));
-      var F = element(by.binding('checkbox.F'));
-      var Sa = element(by.binding('checkbox.Sa'));
+  $scope.saveEntry = function(entry){
+    $scope.selectedEntry.data = $scope.entryCopy.data;
+  }
 
-      expect(Su.getText()).toContain('true');
-      expect(M.getText()).toContain('true');
-      expect(Tu.getText()).toContain('true');
-      expect(W.getText()).toContain('true');
-      expect(Th.getText()).toContain('true');
-      expect(F.getText()).toContain('true');
-      expect(Sa.getText()).toContain('YES');
+  // $scope.submitForm = function(entry){
+  //   $scope.data.push(entry);
+  //   $scope.entry = {};
+  // }
 
-      element(by.model('checkbox.Su')).click();
-      element(by.model('checkbox.M')).click();
-      element(by.model('checkbox.Tu')).click();
-      element(by.model('checkbox.W')).click();
-      element(by.model('checkbox.Th')).click();
-      element(by.model('checkbox.F')).click();
-      element(by.model('checkbox.Sa')).click();
+  $scope.removeEntry = function(entry){
+    var position = $scope.selectedEntry.indexOf(entry);
+    $scope.selectedEntry.splice(position, 1);
+  }
 
-      expect(Su.getText()).toContain('false');
-      expect(M.getText()).toContain('false');
-      expect(Tu.getText()).toContain('false');
-      expect(W.getText()).toContain('false');
-      expect(Th.getText()).toContain('false');
-      expect(F.getText()).toContain('false');
-      expect(Sa.getText()).toContain('NO');
-  }]
-};
+  $scope.entryEdit = function(entry){
+    $scope.entry = entry;
+    $scope.removeEntry(entry);
+  }
+
+});
